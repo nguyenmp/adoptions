4798f13f75086755bc3cf4eeed98a94d89591358
Mark Nguyen <mnguyen4@apple.com>
2020-08-20 14:44:36 -0700

    Page with a URL to streamline the Jenny notification process, also add traceing for which shelter we're looking at cause some of the URLs don't make it obvious

--- adoption.py
+++ adoption.py
@@ -17,22 +17,18 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class Pet(object):
-    def __init__(self, image, name, pet_id, body_func):
+    def __init__(self, image, name, pet_id, url, shelter):
         self.image = image
         self.name = name
-        self.id = pet_id
-        self.body_func = body_func
-        self.file_name = '{}_{}'.format(name, pet_id)
+        self.pet_id = pet_id
+        self.url = url
+        self.shelter = shelter
 
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        return 'Pet(name={}, id={})'.format(self.name, self.id)
-
-    @property
-    def body(self):
-        return self.body_func(self.id)
+        return 'Pet(name={}, shelter={}, url={})'.format(self.name, self.shelter, self.url)
 
 
 def main():
@@ -58,29 +54,32 @@ def has_seen(pet):
 def mark_seen(pet):
     subprocess.check_call(['touch', pet.file_name], cwd=DIRECTORY)
 
+
 def get_pets():
-    LOGGER.info('Querying for all pets')
+    shelter = 'Peninsula Humane Society & SPCA'
+    LOGGER.info('Querying for all %s pets', shelter)
     response = requests.get('https://toolkit.rescuegroups.org/j/3/grid3_layout.php?&toolkitIndex=0&toolkitKey=LQbZuUMn')
     soup = BeautifulSoup(response.text, features="html.parser")
     td_results_cell = soup('td', 'rgtkSearchResultsCell')
-    return [
-        Pet(
-            image=result_cell.div.a.img['src'],
+
+    result = []
+    for result_cell in td_results_cell:
+        image = result_cell.div.a.img['src']
+        pet_id = image.split('/')[-2]
+        pet = Pet(
+            image=image,
             name=result_cell.text.strip(),
-            pet_id=result_cell.div.a.img['src'].split('/')[-2],
-            body_func=get_pet,
+            pet_id=pet_id,
+            url='https://phs-spca.org/adopt/dogs/#action_0=pet&animalID_0={}&petIndex_0=0'.format(pet_id),
+            shelter=shelter,
         )
-        for result_cell in td_results_cell
-    ]
-
-
-def get_pet(pet_id):
-    response = requests.get('https://toolkit.rescuegroups.org/j/3/pet2_layout.php?toolkitIndex=0&toolkitKey=LQbZuUMn&petfocus_0=&resultSort_0=animalUpdatedDate&resultOrder_0=desc&page_0=1&age_0=&sex_0=&searchString_0=&petIndex=6&animalID={}&recentPets=15937621'.format(pet_id))
-    return response.text
+        result.append(pet)
+    return result
 
 
 def get_pets2(page=1):
-    LOGGER.info('Querying for all other pets')
+    shelter = 'Family Dog Rescue'
+    LOGGER.info('Querying for all %s pets', shelter)
     # 'Accept: application/json, text/javascript, */*; q=0.01' \
     data = {
         'gender': '-',
@@ -105,25 +104,24 @@ def get_pets2(page=1):
     if len(data) > 0:
         rest = get_pets2(page + 1)
 
-    result = [
+    result = []
+    for row in data:
+        pet_id = row['animalID']
         Pet(
-            image=pet['animalPictures'][0]['large']['url'],
-            name=pet['animalName'],
-            pet_id=pet['animalID'],
-            body_func=get_pet2)
-        for pet in data
-    ]
+            image=row['animalPictures'][0]['large']['url'],
+            name=row['animalName'],
+            pet_id=pet_id,
+            url='https://www.ilovefamilydog.org/dog-details/?id={}'.format(pet_id),
+            shelter=shelter,
+        )
+
     result.extend(rest)
     return result
 
 
-def get_pet2(pet_id):
-    response = requests.get('https://www.ilovefamilydog.org/dog-details/?id={}'.format(pet_id))
-    soup = BeautifulSoup(response.text, features="html.parser")
-    return soup('p')[-1].text
-
-
 def get_pets3():
+    shelter = 'The Milo Foundation'
+    LOGGER.info('Querying for all %s pets', shelter)
     response = requests.get('https://www.shelterluv.com/available_pets/11413?saved_query=7503&embedded=1&iframeId=shelterluv_embed_114131597715231779&columns=2#https%3A%2F%2Fwww.milofoundation.org%2Fdogs-for-adoption%2F%23sl_embed%26page%3Dshelterluv_embed_114131597715231779%25252Favailable_pets%25252F11413%25253Fsaved_query%25253D7503')
     response.raise_for_status()
     soup = BeautifulSoup(response.text, features="html.parser")
@@ -133,17 +131,11 @@ def get_pets3():
             image=row('img')[0]['src'],
             name=row.text.strip(),
             pet_id=os.path.split(parse.urlparse(row['href']).path)[1],
-            body_func=get_pet3,
+            url=row['href'],
+            shelter=shelter,
         )
         for row in rows
     ]
-
-
-def get_pet3(pet_id):
-    response = requests.get('https://www.shelterluv.com/publish_animal/{}'.format(pet_id))
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, features="html.parser")
-    return soup('p')[0].text
 
 
 def pager_duty(pet, token):
@@ -157,13 +149,13 @@ def pager_duty(pet, token):
     body = {
         "incident": {
             "type": "incident",
-            "title": "New dog alert! {} is now available".format(pet.name),
+            "title": "New dog alert! {} is now available from {}".format(pet.name, pet.shelter),
             "service": {
                 "id": "P1DEO09",
                 "type": "service_reference",
             },
             "body": {
-                "details": pet.body,
+                "details": "{}: {}".format(pet.name, pet.url),
                 "type": "incident_body",
             }
         }
